d9fbc9638f30b28e1138ea59d058c32031823163
jakobln <62882745+jakobln@users.noreply.github.com>
2020-03-30 14:01:15 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Flinter
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            Profile profile1 = new Profile(Profile.Gender.man, Profile.EyeColor.brown, Profile.HairColor.black, Profile.HeightCategory.Short);
+            Profile profile2 = new Profile(Profile.Gender.woman, Profile.EyeColor.blue, Profile.HairColor.blond, Profile.HeightCategory.verytall);
+            Profile profile3 = new Profile(Profile.Gender.man, Profile.EyeColor.green, Profile.HairColor.blond, Profile.HeightCategory.tall);
+
+            /*Profile profile1 = new Profile(true, "Brown", "Black", 1);
+            Profile profile2 = new Profile(false, "White", "Blue", 4);
+            Profile profile3 = new Profile(true, "Blond", "Green", 8);*/
+
+            Console.WriteLine(profile1.Description);
+            Console.WriteLine(profile2.Description);
+            Console.WriteLine(profile3.Description);
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
---
+++ Profile.cs
@@ -0,0 +1,67 @@
+﻿namespace Flinter
+{
+    /// <summary>
+    /// This class represents a person profile,
+    /// for instance for a dating website
+    /// </summary>
+    public class Profile
+    {
+        #region Instance fields
+        private Gender _gender;
+        public enum Gender { man, woman }
+        private EyeColor _eyeColor;
+        public enum EyeColor {blue, brown, green, other}
+        private HairColor _hairColor;
+        public enum HairColor {brown, blond, black, other}
+        private HeightCategory _heightCategory;
+        public enum HeightCategory {veryshort, Short, medium, tall, verytall, unknown}
+        #endregion
+
+        #region Constructor
+        public Profile(Gender gender, EyeColor eyeColor, HairColor hairColor, HeightCategory heightCategory)
+        {
+            _gender = gender;
+            _eyeColor = eyeColor;
+            _hairColor = hairColor;
+            _heightCategory = heightCategory;
+        }
+        #endregion
+
+        #region Properties
+        public string Description
+        {
+            get
+            {
+                return $"You got a {_gender} with {_eyeColor} eyes and {_hairColor} hair, who is {_heightCategory}";
+            }
+        }
+
+        /*public string GenderDescription
+        {
+            get { return _gender ? "man" : "woman"; }
+        }
+
+        public string HeightDescription
+        {
+            get
+            {
+                switch (_heightCategory)
+                {
+                    case 0:
+                        return "Very short";
+                    case 1:
+                        return "Short";
+                    case 2:
+                        return "Medium height";
+                    case 3:
+                        return "Tall";
+                    case 4:
+                        return "Very tall";
+                    default:
+                        return "Unknown height";
+                }
+            }
+        }*/
+        #endregion
+    }
+}
